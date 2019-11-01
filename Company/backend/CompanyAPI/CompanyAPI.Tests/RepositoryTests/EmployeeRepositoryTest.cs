3496ffffa1fa1b6fb9ca84d58a6726af58162724
Kalil Teixeira Ventura Monteiro <kalilventur@gmail.com>
2019-11-01 00:06:28 -0300

    Refatorando testes de unidade

--- Company/backend/CompanyAPI/CompanyAPI.Tests/RepositoryTests/EmployeeRepositoryTest.cs
+++ Company/backend/CompanyAPI/CompanyAPI.Tests/RepositoryTests/EmployeeRepositoryTest.cs
@@ -1,35 +1,33 @@
-﻿using CompanyAPI.Database.Context;
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CompanyAPI.Database.Context;
 using CompanyAPI.Domain.Models;
+using CompanyAPI.Repository.Implementation;
 using CompanyAPI.Repository.Interfaces;
 using Microsoft.EntityFrameworkCore;
 using NSubstitute;
-using System;
-using System.Collections.Generic;
-using System.Text;
 using Xunit;
 
-namespace CompanyAPI.Tests.RepositoryTests
-{
-    public class EmployeeRepositoryTest
-    {
+namespace CompanyAPI.Tests.RepositoryTests {
+    public class EmployeeRepositoryTest {
         private readonly IEmployeeRepository _mockRepository;
         private readonly DbSet<Employee> _mockEmployees;
         private readonly CompanyApiContext _mockContext;
 
-        public EmployeeRepositoryTest()
-        {
-            _mockRepository = Substitute.For<IEmployeeRepository>();
-
+        public EmployeeRepositoryTest () {
+            _mockEmployees = Substitute.For<DbSet<Employee>> ();
+            _mockContext = Substitute.For<CompanyApiContext> ();
+            _mockContext.Employees.Returns(_mockEmployees);
+            _mockRepository = new EmployeeRepository(_mockContext);
         }
 
         [Fact]
-        public void AddEmployee()
-        {
-            int counter = 0;
-            var employee = new Employee("Teste","Document","email@email.com");
-            //_mockRepository.AddAsync(employee);
-            _mockRepository.When(x => x.AddAsync(employee)).Do(x => counter++);
-            Assert.Equal(1, counter);
+        public void AddEmployee () {
+            var employee = new Employee ("Teste", "Document", "email@email.com");
+            _mockRepository.AddAsync(employee);
+            _mockEmployees.Received(1).Add(Arg.Any<Employee>());
+            _mockContext.Received(1).SaveChangesAsync();
         }
     }
 }
