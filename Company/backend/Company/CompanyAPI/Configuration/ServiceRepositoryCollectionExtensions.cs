fbe691e0eb57fd8f9be950d92489337ae4c212b0
Kalil Teixeira Ventura Monteiro <kalilventur@gmail.com>
2019-11-03 00:07:36 -0300

    Melhorias para o swagger no controller Auth

--- Company/backend/Company/CompanyAPI/Configuration/ServiceRepositoryCollectionExtensions.cs
+++ Company/backend/Company/CompanyAPI/Configuration/ServiceRepositoryCollectionExtensions.cs
@@ -9,53 +9,57 @@ using CompanyAPI.Services.Services.Implementation;
 using FluentValidation;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace CompanyAPI.Configuration {
+namespace CompanyAPI.Configuration
+{
     /// <summary>
-    /// Classe criada para a configura��o do DI
+    /// Classe criada para a configuracao do DI
     /// </summary>
-    public static class ServiceRepositoryCollectionExtensions {
+    public static class ServiceRepositoryCollectionExtensions
+    {
         /// <summary>
-        /// M�todo criado para a inser��o dos servi�os e reposit�rios
+        /// M�todo criado para a insercao dos serviços e repositorios
         /// </summary>
-        /// <param name="services">Servi�os da aplica��o</param>
+        /// <param name="services">Servicos da aplicacao</param>
         /// <returns></returns>
-        public static IServiceCollection RegisterRepositoryServices (this IServiceCollection services) {
+        public static IServiceCollection RegisterRepositoryServices(this IServiceCollection services)
+        {
 
             //Services and Repositories
-            services.AddScoped<IAuthService, AuthService> ();
-            services.AddScoped<IAuthRepository, AuthRepository> ();
+            services.AddScoped<IAuthService, AuthService>();
+            services.AddScoped<IAuthRepository, AuthRepository>();
 
-            services.AddScoped<ICompanyService, CompanyService> ();
-            services.AddScoped<ICompanyRepository, CompanyRepository> ();
+            services.AddScoped<ICompanyService, CompanyService>();
+            services.AddScoped<ICompanyRepository, CompanyRepository>();
 
-            services.AddScoped<IEmployeeService, EmployeeService> ();
-            services.AddScoped<IEmployeeRepository, EmployeeRepository> ();
+            services.AddScoped<IEmployeeService, EmployeeService>();
+            services.AddScoped<IEmployeeRepository, EmployeeRepository>();
 
-            services.AddScoped<IEmployeeAddressService, EmployeeAddressService> ();
-            services.AddScoped<IEmployeeAddressRepository, EmployeeAddressRepository> ();
+            services.AddScoped<IEmployeeAddressService, EmployeeAddressService>();
+            services.AddScoped<IEmployeeAddressRepository, EmployeeAddressRepository>();
 
-            services.AddScoped<IAddressService, AddressService> ();
-            services.AddScoped<IAddressRepository, AddressRepository> ();
+            services.AddScoped<IAddressService, AddressService>();
+            services.AddScoped<IAddressRepository, AddressRepository>();
 
             //Generic Repository and Service
-            services.AddScoped (typeof (IBaseService<>), typeof (BaseService<>));
-            services.AddScoped (typeof (IGenericRepository<>), typeof (GenericRepository<>));
+            services.AddScoped(typeof(IBaseService<>), typeof(BaseService<>));
+            services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
 
             return services;
         }
 
         /// <summary>
-        /// Valida��o dos Modelos do projeto
+        /// Validacao dos Modelos do projeto
         /// </summary>
-        /// <param name="services">Servi�os da aplica��o</param>
+        /// <param name="services">Servicos da aplicacao</param>
         /// <returns></returns>
-        public static IServiceCollection RegisterValidators (this IServiceCollection services) {
-            services.AddTransient<IValidator<Login>, LoginValidator> ();
-            services.AddTransient<IValidator<User>, UserValidator> ();
-            services.AddTransient<IValidator<Address>, AddressValidator> ();
-            services.AddTransient<IValidator<Company>, CompanyValidator> ();
-            services.AddTransient<IValidator<Employee>, EmployeeValidator> ();
-            services.AddTransient<IValidator<Register>, RegisterValidator> ();
+        public static IServiceCollection RegisterValidators(this IServiceCollection services)
+        {
+            services.AddTransient<IValidator<Login>, LoginValidator>();
+            services.AddTransient<IValidator<User>, UserValidator>();
+            services.AddTransient<IValidator<Address>, AddressValidator>();
+            services.AddTransient<IValidator<Company>, CompanyValidator>();
+            services.AddTransient<IValidator<Employee>, EmployeeValidator>();
+            services.AddTransient<IValidator<Register>, RegisterValidator>();
 
             return services;
         }
--- Company/backend/Company/CompanyAPI/Controllers/AuthController.cs
+++ Company/backend/Company/CompanyAPI/Controllers/AuthController.cs
@@ -8,56 +8,85 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace CompanyAPI.Controllers
 {
-    [Route ("api/[controller]")]
+    [Route("api/[controller]")]
     [ApiController]
     [AllowAnonymous]
     public class AuthController : ControllerBase
     {
         readonly IAuthService _authService;
 
-        public AuthController (IAuthService authService) {
+        public AuthController(IAuthService authService)
+        {
             _authService = authService;
         }
 
+        /// <summary>
+        /// Responsável pela criação do usuário
+        /// </summary>
+        /// <param name="user">Usuário que será criado</param>
+        /// <returns></returns>
+        /// <response code="201">Usuário criado com sucesso</response>
+        /// <response code="406">Usuário não aceito</response>
+        /// <response code="500">Erro interno</response>
         [HttpPost]
-        [Route ("Register")]
         [AllowAnonymous]
-        public async Task<IActionResult> Register (Register user) {
-            bool userExists = await _authService.UserExists (user.Username);
+        [Route("Register")]
+        [Produces("application/json")]
+        [ProducesResponseType(201)]
+        [ProducesResponseType(406)]
+        [ProducesResponseType(500)]
+        public async Task<IActionResult> Register(Register user)
+        {
+            bool userExists = await _authService.UserExists(user.Username);
 
             if (userExists)
-                return StatusCode (StatusCodes.Status406NotAcceptable, new { message = "Username exists." });
+                return StatusCode(StatusCodes.Status406NotAcceptable, new { message = "Username exists." });
 
-            bool emailExists = await _authService.EmailExists (user.Email);
+            bool emailExists = await _authService.EmailExists(user.Email);
 
             if (emailExists)
-                return StatusCode (StatusCodes.Status406NotAcceptable, new { message = "Email exists." });
+                return StatusCode(StatusCodes.Status406NotAcceptable, new { message = "Email exists." });
 
-            var isRegistered = await _authService.Register (user);
+            var isRegistered = await _authService.Register(user);
 
             if (!isRegistered.Succeeded)
-                return StatusCode (StatusCodes.Status406NotAcceptable, new { message = isRegistered.Errors });
+                return StatusCode(StatusCodes.Status406NotAcceptable, new { message = isRegistered.Errors });
 
-            return StatusCode (StatusCodes.Status201Created, new { message = "User successfully registered." });
+            return StatusCode(StatusCodes.Status201Created, new { message = "User successfully registered." });
         }
 
+        /// <summary>
+        /// Responsável pelo login do usuário
+        /// </summary>
+        /// <param name="user">Usuario que está entrando no sistema.</param>
+        /// <returns></returns>
+        /// <response code="202">Usuário logado com sucesso</response>
+        /// <response code="401">Usuário não autorizado</response>
+        /// <response code="404">Usuário não encontrado</response>
+        /// <response code="500">Erro interno</response>
         [HttpPost]
-        [Route ("Login")]
         [AllowAnonymous]
-        public async Task<IActionResult> Login (Login user) {
-            bool userExists = await _authService.UserExists (user.UserName);
+        [Route("Login")]
+        [Produces("application/json")]
+        [ProducesResponseType(202)]
+        [ProducesResponseType(401)]
+        [ProducesResponseType(404)]
+        [ProducesResponseType(500)]
+        public async Task<IActionResult> Login(Login user)
+        {
+            bool userExists = await _authService.UserExists(user.UserName);
 
             if (!userExists)
-                return StatusCode (StatusCodes.Status404NotFound, new { message = "Username not exists." });
+                return StatusCode(StatusCodes.Status404NotFound, new { message = "Username not exists." });
 
-            var signInUser = await _authService.UserSignIn (user);
+            var signInUser = await _authService.UserSignIn(user);
 
             if (!signInUser.Succeeded)
-                return StatusCode (StatusCodes.Status401Unauthorized, new { message = "Wrong username or password." });
+                return StatusCode(StatusCodes.Status401Unauthorized, new { message = "Wrong username or password." });
 
-            var login = await _authService.Login (user);
+            var login = await _authService.Login(user);
 
-            return StatusCode (StatusCodes.Status202Accepted, new { login });
+            return StatusCode(StatusCodes.Status202Accepted, new { login });
 
         }
     }
