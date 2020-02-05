011325ea9dd9d552751eb54c7154493dd2feb9ba
Kalil Teixeira Ventura Monteiro <kalilventur@gmail.com>
2020-02-04 23:36:29 -0300

    Limpeza da classe DeleteAddress

--- Company/backend/Company/CompanyAPI/DTO/DeleteAddress.cs
+++ Company/backend/Company/CompanyAPI/DTO/DeleteAddress.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace CompanyAPI.DTO
+﻿namespace CompanyAPI.DTO
 {
     public class DeleteAddress
     {
